1668f3f7685c0c28b02cdabf71040f481bbcf040
frimpsss <afrimpong031@gmail.com>
2024-07-29 04:25:52 +0000

    update

--- main.py
+++ main.py
@@ -60,6 +60,43 @@ async def receive_card_data(card_id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.get("/first_false_room")
+async def get_all_rooms():
+    # Retrieve the allowed rooms
+    allowed_rooms = db.child("allowedRooms").get().val()
+    tobeReturned = None
+    # Check if the card is allowed for any room
+    for  room in allowed_rooms.items():
+        if room[1]['isAssigned'] is False:
+            print(room)
+            tobeReturned = room
+            break
+            
+
+    if tobeReturned is None:
+        tobeReturned = {"room": None, "access": False}
+    
+    return {"room": tobeReturned}
+
+@app.post("/change_first_false_room")
+async def change_first_false_room():
+    # Retrieve the allowed rooms
+    allowed_rooms = db.child("allowedRooms").get().val()
+    tobeReturned = None
+    # Check if the card is allowed for any room
+    for  room in allowed_rooms.items():
+        if room[1]['isAssigned'] is False:
+            db.child("allowedRooms").child(room[0]).update({"isAssigned": True})
+            break
+            
+
+    if tobeReturned is None:
+        tobeReturned = {"room": None, "access": False}
+    
+    return {"room": tobeReturned}
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Card Data and Room Confirmation API"}
